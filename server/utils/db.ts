24237652db9cfc54492b3c57b90e21535de5078e
alsaze <veselov@alfabit.com>
2025-12-10 13:36:35 +0300

    init prysma

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,7 +1,7 @@
-import { PrismaLibSql } from '@prisma/adapter-libsql'
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
 import { PrismaClient } from '../../prisma/generated/client'
 
-const adapter = new PrismaLibSql({
+const adapter = new PrismaBetterSqlite3({
   url: process.env.DATABASE_URL ?? '',
 })
 
